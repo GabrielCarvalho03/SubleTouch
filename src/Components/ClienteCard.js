312b87c02254ae19338636dd4d7a876b74e114a0
Gabriel <gabriel.carvalho@beelegal.com>
2024-09-10 14:35:06 -0300

    resolved bug

--- src/Components/ClienteCard.js
+++ src/Components/ClienteCard.js
@@ -1,35 +1,45 @@
-import { Title,Box , Container } from "../Css/global";
+import { Title, Box, Card } from "../Css/global";
 
-import Roberta from '../Assets/Roberta.png'
-import Rodrigo from '../Assets/Rodrigo.png'
-import Renan from '../Assets/Renan.png'
-import stars from '../Assets/stars.png'
-const ClienteCard = ({name , text , profile , classe , classegeral}) => {
-    return(
-     
-    <Box top={10}  left={10} className={ classegeral ?  classegeral :"boxclientcard" }>
-        <Box row>
-            <Box left={3} top={3}>
-            <img src={profile == 'roberta' ? Roberta : profile== 'rodrigo' ? Rodrigo : profile =='renan' ? Renan : '' }/>
+import Roberta from "../Assets/Roberta.png";
+import Rodrigo from "../Assets/Rodrigo.png";
+import Renan from "../Assets/Renan.png";
+import stars from "../Assets/stars.png";
 
-            </Box>
-                <Title top={-5}  className="cardname"   left={5} size={22}>{name}</Title>
-
-
+const ClienteCard = ({ name, text, profile, classe, classegeral }) => {
+  return (
+    <Box
+      top={10}
+      left={1}
+      className={classegeral ? classegeral : "boxclientcard"}
+    >
+      <Box row>
+        <Box left={3} top={4}>
+          <img
+            src={
+              profile == "roberta"
+                ? Roberta
+                : profile == "rodrigo"
+                ? Rodrigo
+                : profile == "renan"
+                ? Renan
+                : ""
+            }
+          />
         </Box>
-        <Box top={-8} left={25} className="boxstarts">
-        <img src={stars}/>
-        </Box>
-        <Box  top={5}>
-        <Title className={`${classe}`} opacity size={22}>
-      {text}
+        <Title top={-5} className="cardname" left={5} size={22}>
+          {name}
         </Title>
-        </Box>
-       
+      </Box>
+      <Box top={-8} left={25} className="boxstarts">
+        <img src={stars} />
+      </Box>
+      <Box top={5} padding={"0 10px"}>
+        <Title className={`${classe}`} opacity size={20}>
+          {text}
+        </Title>
+      </Box>
     </Box>
-
-     
-    )
-}
+  );
+};
 
 export default ClienteCard;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,8 @@
-import {Box, Container} from '../Css/global'
+import { Box, Container } from "../Css/global";
 
-import logoout from '../Assets/logoout.svg'
-import SolarEnergy from '../Assets/SolarEnergy.png'
-import rowup from '../Assets/rowup.png'
-
+import logoout from "../Assets/logoout.svg";
+import SolarEnergy from "../Assets/SolarEnergy.png";
+import rowup from "../Assets/rowup.png";
 
 import Header from "../Components/Header";
 import Diferentials from "../Components/Diferentials";
@@ -15,55 +14,44 @@ import Team from "../Components/Team";
 import Opinion from "../Components/Opinion";
 import Footer from "../Components/Footer";
 
-
-
-
 const Home = () => {
-
-
-    return(
-        <>
-        
-        {window.document.body.clientWidth <=850 ?
-            <Box  className="boxmenuMobile" >
-           <Container>
-               <Box row justify="space-between" >
+  return (
+    <>
+      {window.document.body.clientWidth <= 850 ? (
+        <Box className="boxmenuMobile">
+          <Container>
+            <Box row justify="space-between">
+              <Box>
+                <img src={logoout} className="logomenumobile" />
+              </Box>
+              <Box row>
                 <Box>
-                    <img  src={logoout} className="logomenumobile" />
-                </Box>
-                <Box row  >
-
-                  <Box>
-                      <img src={SolarEnergy} className="iconmenumobile" />
-                  </Box>
-
-                  <Box left={20}>
-                      <img src={rowup} className="iconmenumobile" />
-                  </Box>
+                  <img src={SolarEnergy} className="iconmenumobile" />
                 </Box>
 
-               </Box>
-           </Container>
+                <Box left={20}>
+                  <img src={rowup} className="iconmenumobile" />
+                </Box>
+              </Box>
             </Box>
-        : ''}
-        <Menu/>
-        
-        <Header/>
-        <Diferentials/>
-        <Rel/>
-        <Services/>
-        <Team/>
-        <Opinion/>
-        {/* Menu Mobile   */}
+          </Container>
+        </Box>
+      ) : (
+        ""
+      )}
+      <Menu />
 
-        <Footer/>
-       
-       
-        </>
+      <Header />
+      <Diferentials />
+      <Rel />
+      <Services />
+      <Team />
+      <Opinion />
+      {/* Menu Mobile   */}
 
-  
-   
-    )
-}
+      <Footer />
+    </>
+  );
+};
 
 export default Home;
